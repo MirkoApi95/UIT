4b841d7be7b5f598fa9bb1c810c7a0fc28ed738d
Mirko <mirko.apicella1995@gmail.com>
2018-01-05 12:33:00 +0100

    Metodo lista associazioni tu ta

--- UIT/src/model/Associazionetu_taDao.java
+++ UIT/src/model/Associazionetu_taDao.java
@@ -14,44 +14,54 @@ public class Associazionetu_taDao {
 
 	//****VARIABILI DELLA CLASSE****\
 
-		private Connection connection = null;
-		private PreparedStatement preparedStatement = null;
-	
-		//****QUERY****\\		
-		private final String listaIDAssociati = "SELECT tutoruniversitario_utente_id_Utente FROM"+
-											  "associazionetu_ta WHERE tutorAziendale1_utente_id_Utente=?";
-		
-		private final String listaTUAssociati ="SELECT Nome FROM Utente"
-		
-		//****COSTRUTTORE****\\
-		public Associazionetu_taDao() throws SQLException{
-			connection=DriverManagerConnectionPoolUIT.getConnection();
+	private Connection connection = null;
+	private PreparedStatement preparedStatement = null;
+	private ArrayList<TutorUniversitario>lista =new ArrayList<TutorUniversitario>();
+	private TutorUniversitario tu;
+
+
+	//****QUERY****\\		
+	private final String listaIDAssociati = "SELECT tutoruniversitario_utente_id_Utente FROM"+
+			"associazionetu_ta WHERE tutorAziendale1_utente_id_Utente=?";
+
+	private final String listaTUAssociati ="SELECT Nome,Cognome FROM Utente WHERE id_Utente =?";
+
+	//****COSTRUTTORE****\\
+	public Associazionetu_taDao() throws SQLException{
+		connection=DriverManagerConnectionPoolUIT.getConnection();
+	}
+	//****CHIUSURA DELLA CONNESSIONE\\
+	public boolean ChiudiConnessione(){
+		try {
+			return DriverManagerConnectionPoolUIT.releaseConnection(connection);
+		}catch (SQLException e1) {
+			e1.printStackTrace();
 		}
-		//****CHIUSURA DELLA CONNESSIONE\\
-		public boolean ChiudiConnessione(){
-			try {
-				return DriverManagerConnectionPoolUIT.releaseConnection(connection);
-			}catch (SQLException e1) {
-				e1.printStackTrace();
+		return false;
+	}
+
+
+	//****LISTA ASSOCIAZIONI
+	public ArrayList<TutorUniversitario> listaAssociazioniTU_TA(TutorAziendale TA){
+		try {
+			preparedStatement=connection.prepareStatement(listaIDAssociati);
+			preparedStatement.setInt(1, TA.getId());
+			ResultSet rs=preparedStatement.executeQuery();
+			while(rs.next()) {
+				tu.setId(rs.getInt(1));
+				lista.add(tu);
 			}
-			return false;
-		}
-		
-		
-		//****LISTA ASSOCIAZIONI
-		public ArrayList<TutorUniversitario> listaAssociazioniTU_TA(TutorAziendale TA){
-			
-			ArrayList<String>lista =new ArrayList<String>();
-			try {
-				preparedStatement=connection.prepareStatement(listaAssociazioniTU_TA);
-				ResultSet rs=preparedStatement.executeQuery();
-				while(rs.next()) {
-					lista.add(rs.getString(1));
-				}
-				return lista;
-			}catch (SQLException e) {
-				e.printStackTrace();
-				return null;
+			connection.prepareStatement(listaTUAssociati);
+			ResultSet rs1=preparedStatement.executeQuery();
+
+			for(int i=0; i<lista.size(); i++) {
+				lista.get(i).setNome(rs1.getString(1));
+				lista.get(i).setCognome(rs1.getString(2));
 			}
+			return lista;
+		}catch (SQLException e) {
+			e.printStackTrace();
+			return null;
 		}
+	}
 }
