50e63db19d7261180b26ac1d6ccece0e26b48e0c
Salvatore <amsalvatore91@gmail.com>
2018-01-18 13:39:36 +0100

    Correzioni testing

--- UIT/src/seleniumTesting/TC_1_2_1.java
+++ UIT/src/seleniumTesting/TC_1_2_1.java
@@ -12,12 +12,15 @@ public class TC_1_2_1 {
    * La calsse testa il login con i dati corretti
    */
   public static void main(String[] args) {
-    System.setProperty("webdriver.chrome.driver", "D:\\Utenti\\Mirko\\Desktop\\chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Salvatore\\Desktop\\SELENIUM\\chromedriver.exe");
     WebDriver driver= new ChromeDriver();
     driver.get("http://localhost:8080/UIT/HomePageViewGenerale.jsp");
     WebElement inputEmail;
     WebElement inputpassword;
     WebElement button;
+    WebElement menu;
+    menu= driver.findElement(By.id("log"));
+    menu.click();
     inputEmail= driver.findElement(By.id("emailId"));
     inputEmail.sendKeys("s.apicella22@studenti.unisa.it");
     inputpassword= driver.findElement(By.id("passwordinput"));
