043b57b00ef7c612b1948ec3e28979118a044d63
Mirko <mirko.apicella1995@gmail.com>
2018-01-18 16:32:50 +0100

    Test case

--- UIT/src/seleniumTesting/TC_1_3_4.java
+++ UIT/src/seleniumTesting/TC_1_3_4.java
@@ -13,16 +13,37 @@ public class TC_1_3_4 {
      */
     System.setProperty("webdriver.chrome.driver", "D:\\Utenti\\Mirko\\Desktop\\chromedriver.exe");
     WebDriver driver= new ChromeDriver();
-    driver.get("http://localhost:8080/UIT/AccountView.jsp");
+    driver.get("http://localhost:8080/UIT/HomePageViewGenerale.jsp");
+    WebElement inputEmail;
     WebElement inputpassword;
-    WebElement inputnewpassword;
     WebElement button;
-
+    WebElement menu;
+    menu= driver.findElement(By.id("log"));
+    menu.click();
+    inputEmail= driver.findElement(By.id("emailId"));
+    inputEmail.sendKeys("s.apicella22@studenti.unisa.it");
     inputpassword= driver.findElement(By.id("passwordinput"));
-    inputpassword.sendKeys("apicella55");
-    inputnewpassword= driver.findElement(By.id("confirm_password"));
-    inputnewpassword.sendKeys("");
-    button=driver.findElement(By.id("conferma"));
+    inputpassword.sendKeys("apicella22");
+    button=driver.findElement(By.id("entra"));
     button.click();
+  
+    try {
+      Thread.sleep(7000);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  
+    driver.get("http://localhost:8080/UIT/AccountView.jsp");
+    WebElement inputpassword1;
+    WebElement inputnewpassword1;
+    WebElement button1 = null;
+
+    inputpassword1= driver.findElement(By.id("passwordinput"));
+    inputpassword1.sendKeys("apicella22");
+    inputnewpassword1= driver.findElement(By.id("confirm_password"));
+    inputnewpassword1.sendKeys("");
+    button1=driver.findElement(By.id("confermapassword"));
+    button1.click();
   }
 }
